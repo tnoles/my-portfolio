fcb247984bc15101b8c3569b190a0fdfc1b997c0
Thaniel Noles <tnoles@eplus.com>
2018-10-25 13:22:35 -0700

    add upload script for Lambda

--- upcloudlamb.py
+++ upcloudlamb.py
@@ -3,16 +3,20 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-cloudbit_bucket = s3.Bucket('cloudbit.tv')
-build_bucket =s3.Bucket('buildcloudbit')
+def lambda_handler(event, context):
 
-do_zip=StringIO.StringIO()
-build_bucket.download_fileobj('buildcloudbit.zip', do_zip)
+    s3 = boto3.resource('s3')
 
-with zipfile.ZipFile(do_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        cloudbit_bucket.upload_fileobj(obj,nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        cloudbit_bucket.Object(nm).Acl().put(ACL='public-read')
+    cloudbit_bucket = s3.Bucket('cloudbit.tv')
+    build_bucket =s3.Bucket('buildcloudbit')
+
+    do_zip=StringIO.StringIO()
+    build_bucket.download_fileobj('buildcloudbit.zip', do_zip)
+
+    with zipfile.ZipFile(do_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            cloudbit_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            cloudbit_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Hello from Lambda'
