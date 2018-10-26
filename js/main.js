1dde71c3c0f30540830d4cb34c43a2b94d79b499
Thaniel Noles <tnoles@eplus.com>
2018-10-26 13:07:43 -0700

    configured webpack

---
+++ js/main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+
+const myWork = [
+  {
+    'title': "Portfolio Boilerplate",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/c1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/c2.png",
+      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/c3.png",
+      'comment': ""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
