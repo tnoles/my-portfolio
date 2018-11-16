fb75658ef931a91a9bd098804b434e4234d2b753
Thaniel Noles <tnoles@eplus.com>
2018-11-15 21:19:26 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ console.log("Webpack works");
 const myWork = [
   {
     'title': "Work Example",
-    'href'; "https://example.com",
+    'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "A Serverless Portfolio",
@@ -16,7 +16,7 @@ const myWork = [
   },
   {
     'title': "CloudFormation",
-    'href'; "https://example.com",
+    'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving cats",
@@ -27,7 +27,7 @@ const myWork = [
   },
   {
     'title': "Lambda Functions",
-    'href'; "https://example.com",
+    'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving code",
